c5764a3b01c99149cc18ebe9ca62e56592955a8b
ulkiobal <ulkiobal@users.noreply.github.com>
2021-01-29 18:42:55 +0100

    Modelo creado, junto con dependencias

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\web;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\web  $web
+     * @return \Illuminate\Http\Response
+     */
+    public function show(web $web)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\web  $web
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(web $web)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\web  $web
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, web $web)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\web  $web
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(web $web)
+    {
+        //
+    }
+}
---
+++ database/factories/WebFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\web;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WebFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = web::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
